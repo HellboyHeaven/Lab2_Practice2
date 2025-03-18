7878d08e1b6fc83a80ffae878139ec74f39cfbac
HellboyHeavens <49723005+HellboyHeaven@users.noreply.github.com>
2025-03-18 19:56:44 +0100

    done

--- GpsNavigation/GpsNavigation/Waypoint.cs
+++ GpsNavigation/GpsNavigation/Waypoint.cs
@@ -2,5 +2,23 @@
 
 public class Waypoint
 {
-    //Your code here
+    public string Name { get;}
+
+    public Coordinates Coordinates { get; }
+    public (double, double) GetCoordinates() => (Coordinates.Latitude, Coordinates.Longitude);
+
+    public Waypoint(string name, double latitude, double longitude)
+    {
+        Name = name;
+        Coordinates = new Coordinates(latitude, longitude);
+    }
+
+
+    public void Deconstruct(out double latitude, out double longitude)
+    {
+        latitude = Coordinates.Latitude;
+        longitude = Coordinates.Longitude;
+    }
 }
+
+public record Coordinates(double Latitude, double Longitude);
